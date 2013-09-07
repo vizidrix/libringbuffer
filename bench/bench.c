d63a1b3168cb3686b8dc13fdb2e772d2f8f7ce60
Perry Birch <perrybirch@vizidrix.com>
2013-09-07 02:34:37 -0700

    Trying to fix build

--- bench/bench.c
+++ bench/bench.c
@@ -8,17 +8,26 @@
 void bench_claim_publish_release(B * b) {
 	int i;
 
+	rb_ringbuffer * buffer;
+	uint64_t seq_num = 0;
+	//char * entry;
+
+	rb_init(&buffer, 4, 1024);
+
 	b_start_timer(b);
-
-	for(i = 0; i < b_count(b); b_sample(b, ++i)) {
-
+	for(i = 0; i < b_count(b); b_sample(b, ++i)) {		
+		rb_claim(buffer, &seq_num, 1);
+		rb_get(buffer, &entry, seq_num);
+		rb_publish(buffer, seq_num, 1);
+		rb_release(buffer, seq_num, 1);
 	}
-
 	b_stop_timer(b);
+
+	rb_free(&buffer);
 }
 
 int main() {
-	printf("\nlibringbuffer - bench\n\n");
+	printf("\nlibringbuffer - bench2\n\n");
 	
 	BENCH(1000, "Claim-Publish-Release", &bench_claim_publish_release);
 
--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /c/vizidrix/src/github.com/vizidrix/libringbuffer/test
-# Build directory: /c/vizidrix/src/github.com/vizidrix/libringbuffer/build/test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(libringbuffer_test "/c/vizidrix/src/github.com/vizidrix/libringbuffer/build/bin/libringbuffer_test")
